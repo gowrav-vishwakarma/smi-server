915d17ab11e2684a525b189fded22d76d4555b47
sinharakesh <rksinha.btech@gmail.com>
2023-07-07 15:36:19 +0530

    comment isQuestionSolved isOptional

--- src/api/dto/create-comment.dto.ts
+++ src/api/dto/create-comment.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsMongoId, IsString } from 'class-validator';
+import { IsBoolean, IsMongoId, IsOptional, IsString } from 'class-validator';
 
 export class CreateCommentDTO {
   @IsMongoId()
@@ -14,6 +14,7 @@ export class CreateCommentDTO {
   video: string | null;
 
   @IsBoolean()
+  @IsOptional()
   @ApiProperty({ default: false })
   isQuestionSolved: boolean;
 }
