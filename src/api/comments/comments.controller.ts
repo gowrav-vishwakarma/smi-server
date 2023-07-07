9bb7a24a86facc2726cb6cb8df74bede26105f59
sinharakesh <rksinha.btech@gmail.com>
2023-07-07 19:59:01 +0530

    comment with screen recording upload feature added

--- src/api/comments/comments.controller.ts
+++ src/api/comments/comments.controller.ts
@@ -7,28 +7,45 @@ import {
   UseGuards,
   UsePipes,
   ValidationPipe,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { CreateCommentDTO } from '../dto/create-comment.dto';
 import { VoteCommentDTO } from '../dto/vote-comment.dto';
 import { UserDocument } from '../schemas/user.schema';
 import { CommentsService } from './comments.service';
+import { Express } from 'express';
+import { MediaService } from '../media/media.service';
 
 @Controller('comments')
 @ApiTags('Comments')
 export class CommentsController {
-  constructor(private readonly commentsService: CommentsService) {}
+  constructor(
+    private readonly commentsService: CommentsService,
+    private readonly mediaService: MediaService,
+  ) {}
 
   @Post('/create')
   @UsePipes(ValidationPipe)
   @ApiBearerAuth()
+  @UseInterceptors(FileInterceptor('video'))
   @UseGuards(AuthGuard())
   async createComment(
     @GetUser() user: UserDocument,
     @Body() createCommentDTO: CreateCommentDTO,
+    @UploadedFile() video: Express.Multer.File,
   ) {
+    if (video) {
+      const mediaRes = await this.mediaService.createMedia(
+        video,
+        `questions/${createCommentDTO.questionId}/comments/${createCommentDTO.questionId}_video.webm`,
+      );
+      createCommentDTO['video'] = mediaRes['key'];
+    }
     return await this.commentsService.createComment(createCommentDTO, user);
   }
 
